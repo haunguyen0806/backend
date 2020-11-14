1c302150ae6879d38c65da38c960b8935fe813b8
haunguyen0806 <nguusaongu@gmail.com>
2020-11-14 20:44:55 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "sql12.freemysqlhosting.net",
+    USER: "sql12376289",
+    PASSWORD: "g1MGmxEfP4",
+    DB: "sql12376289",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+};
